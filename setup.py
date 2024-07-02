8ca805a23f6b994b2ac0b81223b6aa1bebffdac9
lugo <jsdmail@gmail.com>
2024-07-02 12:13:09 +0100

    reqs and setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+# Read requirements.txt and store its contents in a list
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='realtimediffusion',
+    version='0.1',
+    url='https://github.com/lunarring/real_time_diffusion',
+    description='Real time diffusion',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'lunar_tools @ git+https://github.com/lunarring/lunar_tools.git#egg=lunar_tools'
+    ] + required,
+    include_package_data=False,
+)
+
+
