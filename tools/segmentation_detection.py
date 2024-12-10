cebb60ad5fcf6bb0eb81cb05c848495018ac58ab
Your Name <jsdmail@gmail.com>
2024-12-10 12:02:38 +0000

    multi person face cropping (n=2)

--- tools/segmentation_detection.py
+++ tools/segmentation_detection.py
@@ -138,12 +138,14 @@ class HumanSeg:
 
 
 class FaceCropper:
-    def __init__(self, model_path=None, padding=30):
+    def __init__(self, model_path=None, padding=30, single_mode=True):
         if model_path is None:
             model_path = hf_hub_download(repo_id="arnabdhar/YOLOv8-Face-Detection", filename="model.pt")
         self.yolo = YOLO(model_path)
         self.padding = int(padding)
         self.nmb_faces_present = 0
+        self.single_mode = single_mode
+        
 
     def set_padding(self, value):
         # print(f"set_padding called with {value}")
@@ -151,14 +153,24 @@ class FaceCropper:
             raise ValueError("Padding must be non-negative")
         self.padding = int(value)
 
-    def get_cropping_coordinates(self, input_img):
-        # import pdb; pdb.set_trace()
+    def convert_input_img(self, input_img):
         if isinstance(input_img, torch.Tensor):
             input_img = input_img.cpu().numpy()
             input_img = np.clip(input_img * 255, 0, 255)
             
+        if isinstance(input_img, Image.Image):
+            input_img = np.array(input_img)
+
         if len(input_img.shape)==4:
             input_img = input_img[0, :, :, :]
+            
+        return input_img
+        
+
+
+    def get_cropping_coordinates(self, input_img):
+        input_img = self.convert_input_img(input_img)
+
 
     
         # Run face detection
@@ -174,75 +186,105 @@ class FaceCropper:
                 return None
     
             # Initialize variables to store the maximum area and corresponding index
-            max_area = 0
             cropping_coordinates = None
+            list_cropping_coordinates = []
     
-            # Loop through all detected faces
-            for result in face_results:
-                # Retrieve the bounding box coordinates in the format (x1, y1, x2, y2)
-                x1, y1, x2, y2 = result.boxes.xyxy[0].cpu().numpy()
-                # Calculate the area of the bounding box
-                area = (x2 - x1) * (y2 - y1)
+            if self.single_mode:
+                max_area = 0
+                # Loop through all detected faces and find largest one!
+                for result in face_results:
+                    # Retrieve the bounding box coordinates in the format (x1, y1, x2, y2)
+                    x1, y1, x2, y2 = result.boxes.xyxy[0].cpu().numpy()
+                    # Calculate the area of the bounding box
+                    area = (x2 - x1) * (y2 - y1)
+        
+                    # Check if this area is the largest we've seen so far
+                    if area > max_area:
+                        max_area = area
+                        # Update the cropping coordinates to the largest face found
+                        cropping_coordinates = (x1, y1, x2, y2)
     
-                # Check if this area is the largest we've seen so far
-                if area > max_area:
-                    max_area = area
-                    # Update the cropping coordinates to the largest face found
-                    cropping_coordinates = (x1, y1, x2, y2)
-    
-            if cropping_coordinates is None:
-                return None
-    
-            x1, y1, x2, y2 = cropping_coordinates
-    
-            # Adjust to square
-            width = x2 - x1
-            height = y2 - y1
-    
-            if width > height:
-                difference = (width - height) // 2
-                y1 = max(0, y1 - difference)
-                y2 = min(input_img.shape[0], y2 + difference)
-            elif height > width:
-                difference = (height - width) // 2
-                x1 = max(0, x1 - difference)
-                x2 = min(input_img.shape[1], x2 + difference)
-    
-            if (x2 - x1) != (y2 - y1):
-                new_size = min(x2 - x1, y2 - y1)
-                x2 = x1 + new_size
-                y2 = y1 + new_size
-    
-            # Calculate new padding to center the padding around the box
-            padding_x = (self.padding // 2, self.padding // 2)
-            padding_y = (self.padding // 2, self.padding // 2)
-    
-            # Adjust padding if it causes the box to go out of image bounds
-            if x1 - padding_x[0] < 0:
-                padding_x = (x1, self.padding - x1)
-            if x2 + padding_x[1] > input_img.shape[1]:
-                padding_x = (self.padding - (input_img.shape[1] - x2), input_img.shape[1] - x2)
-    
-            if y1 - padding_y[0] < 0:
-                padding_y = (y1, self.padding - y1)
-            if y2 + padding_y[1] > input_img.shape[0]:
-                padding_y = (self.padding - (input_img.shape[0] - y2), input_img.shape[0] - y2)
-    
-            # Apply adjusted padding
-            x1 = max(0, x1 - padding_x[0])
-            x2 = min(input_img.shape[1], x2 + padding_x[1])
-            y1 = max(0, y1 - padding_y[0])
-            y2 = min(input_img.shape[0], y2 + padding_y[1])
-    
-            cropping_coordinates = (int(x1), int(y1), int(x2), int(y2))
-    
-            return cropping_coordinates
+                if cropping_coordinates is None:
+                    return None
+                else:
+                    list_cropping_coordinates.append(cropping_coordinates)
+
+            else:
+                # Take the first two faces and assign the left one to list_cropping_coordinates[0] and right one to list_cropping_coordinates[1]
+                if self.nmb_faces_present >= 2:
+                    result = face_results[0]
+                    x1_first, y1_first, x2_first, y2_first = result.boxes.xyxy[0].cpu().numpy()
+                    x1_second, y1_second, x2_second, y2_second = result.boxes.xyxy[1].cpu().numpy()
+
+                    # Measure x positions to determine which is left and which is right
+                    if x1_first < x1_second:
+                        list_cropping_coordinates.append((x1_first, y1_first, x2_first, y2_first))
+                        list_cropping_coordinates.append((x1_second, y1_second, x2_second, y2_second))
+                    else:
+                        list_cropping_coordinates.append((x1_second, y1_second, x2_second, y2_second))
+                        list_cropping_coordinates.append((x1_first, y1_first, x2_first, y2_first))
+                else:
+                    return [None, None]
+
+
+            list_cropping_coordinates_fixed = []
+            for cropping_coordinates in list_cropping_coordinates:
+
+                x1, y1, x2, y2 = cropping_coordinates
+        
+                # Adjust to square
+                width = x2 - x1
+                height = y2 - y1
+        
+                if width > height:
+                    difference = (width - height) // 2
+                    y1 = max(0, y1 - difference)
+                    y2 = min(input_img.shape[0], y2 + difference)
+                elif height > width:
+                    difference = (height - width) // 2
+                    x1 = max(0, x1 - difference)
+                    x2 = min(input_img.shape[1], x2 + difference)
+        
+                if (x2 - x1) != (y2 - y1):
+                    new_size = min(x2 - x1, y2 - y1)
+                    x2 = x1 + new_size
+                    y2 = y1 + new_size
+        
+                # Calculate new padding to center the padding around the box
+                padding_x = (self.padding // 2, self.padding // 2)
+                padding_y = (self.padding // 2, self.padding // 2)
+        
+                # Adjust padding if it causes the box to go out of image bounds
+                if x1 - padding_x[0] < 0:
+                    padding_x = (x1, self.padding - x1)
+                if x2 + padding_x[1] > input_img.shape[1]:
+                    padding_x = (self.padding - (input_img.shape[1] - x2), input_img.shape[1] - x2)
+        
+                if y1 - padding_y[0] < 0:
+                    padding_y = (y1, self.padding - y1)
+                if y2 + padding_y[1] > input_img.shape[0]:
+                    padding_y = (self.padding - (input_img.shape[0] - y2), input_img.shape[0] - y2)
+        
+                # Apply adjusted padding
+                x1 = max(0, x1 - padding_x[0])
+                x2 = min(input_img.shape[1], x2 + padding_x[1])
+                y1 = max(0, y1 - padding_y[0])
+                y2 = min(input_img.shape[0], y2 + padding_y[1])
+        
+                cropping_coordinates_fixed = (int(x1), int(y1), int(x2), int(y2))
+                list_cropping_coordinates_fixed.append(cropping_coordinates_fixed)
+
+            if self.single_mode:
+                return list_cropping_coordinates_fixed[0]
+            else:
+                return list_cropping_coordinates_fixed
             
         except Exception as e:
             # print(f"get_cropping_coordinates exception {e}")
             return None
         
     def apply_crop(self, input_img, cropping_coordinates):
+        input_img = self.convert_input_img(input_img)
         if cropping_coordinates:
             return Image.fromarray(input_img).crop(cropping_coordinates)
         else:
@@ -268,24 +310,27 @@ if __name__ == '__main__':
     # input_img = np.expand_dims(input_img, axis=0)
     
     
-    shape_cam=(300,400) 
-    cam = lt.WebCam(cam_id=2, shape_hw=shape_cam)
+    # shape_cam=(1080, 1920) 
+    # cam = lt.WebCam(shape_hw=shape_cam)
 
-    face_cropper = FaceCropper(padding=30)
-    
-    
-    img_cam = cam.get_img()
+    face_cropper = FaceCropper(padding=30, single_mode=False)
+    img_cam = Image.open('/home/lugo/git/tmp/img_twoface/img2.jpg')
     cropping_coordinates = face_cropper.get_cropping_coordinates(img_cam)
+    face_cropper.apply_crop(img_cam, cropping_coordinates[0])
+    
 
-    if cropping_coordinates is not None:
-        cam_img_cropped = Image.fromarray(img_cam).crop(cropping_coordinates)
+    #%%
+    # cropping_coordinates = face_cropper.get_cropping_coordinates(img_cam)
 
-        # Display the cropped image
-        plt.imshow(cam_img_cropped)
-        plt.ion()
-        plt.show()
-    else:
-        print("No face detected.")
+    # if cropping_coordinates is not None:
+    #     cam_img_cropped = Image.fromarray(img_cam).crop(cropping_coordinates)
+
+    #     # Display the cropped image
+    #     plt.imshow(cam_img_cropped)
+    #     plt.ion()
+    #     plt.show()
+    # else:
+    #     print("No face detected.")
 
 
 
